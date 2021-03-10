16c8116d00aade2fd2004ae1ee2bec3d1867ef47
angel <angel@DESKTOP-R1E2C95.myfiosgateway.com>
2021-03-10 15:44:29 -0500

    Roach population and simulation

---
+++ HelloWorld2/src/RoachPopulation.java
@@ -0,0 +1,21 @@
+
+public class RoachPopulation {
+	double population;
+	
+	public RoachPopulation(int initialPop) {
+		population = initialPop;
+	}
+	public void breed() {
+		population = population * 2;
+	}
+		
+	public void spray(double sprayPct) {
+		population = population - (population * sprayPct);
+		
+	}
+	public double getRoaches() {
+		return population;
+	}
+
+}
+
