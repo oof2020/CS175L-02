8cd69c8f3ac5155aa97a381f82022cd51cc4e82e
oof2020 <71726040+oof2020@users.noreply.github.com>
2021-05-06 12:13:17 -0400

    Multiplication Java correct

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -2,19 +2,16 @@
 public class MultiplicationTable {
      
     public static void main(String[] args) {
-       int tablesize = 10; 
-   System.out.println(tablesize);
-    }
-
-	public static void MultiplicationTable(int tablesize) {
+       
+		int tablesize=10;
 		for(int i = 1; i<=tablesize;i++ ) {
 			
         	for(int j=1;j<=tablesize;j++) {
                 System.out.format("%4d",i*j);
         	}
             System.out.println();
-            }
-        }
+            
+		}
 	}
     
 } 
